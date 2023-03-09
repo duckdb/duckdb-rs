fca3e005dc036173686f42a793979dda6e258133
wangfenjin <wangfenj@gmail.com>
2023-03-09 22:24:47 +0800

    fix upgrade.sh
    Change-Id: I191f8de3f60bea6ff1c1c441df8418c61f3510ae

--- libduckdb-sys/upgrade.sh
+++ libduckdb-sys/upgrade.sh
@@ -11,20 +11,22 @@ export DU_INCLUDE_DIR="$DUCKDB_LIB_DIR"
 
 # Download and extract amalgamation
 DUCKDB_VERSION=v0.7.1
-wget -T 20 "https://github.com/duckdb/duckdb/releases/download/$DUCKDB_VERSION/libduckdb-src.zip"
-unzip -o libduckdb-src.zip -d duckdb
-rm -f libduckdb-src.zip
+git submodule update --init
+cd "$SCRIPT_DIR/duckdb-sources" || { echo "fatal error" >&2; exit 1; }
+git checkout "$DUCKDB_VERSION"
+cd "$SCRIPT_DIR" || { echo "fatal error" >&2; exit 1; }
+python "$SCRIPT_DIR/update_sources.py"
 
 # Regenerate bindgen file for DUCKDB
 rm -f "$DUCKDB_LIB_DIR/bindgen_bundled_version.rs"
 cargo update
 # Just to make sure there is only one bindgen.rs file in target dir
 find "$SCRIPT_DIR/../target" -type f -name bindgen.rs -exec rm {} \;
-env LIBDUCKDB_SYS_BUNDLING=1 cargo test --features "bundled buildtime_bindgen"
+env LIBDUCKDB_SYS_BUNDLING=1 cargo test --features "extensions-full buildtime_bindgen"
 find "$SCRIPT_DIR/../target" -type f -name bindgen.rs -exec cp {} "$DUCKDB_LIB_DIR/bindgen_bundled_version.rs" \;
 
 # Sanity checks
 cd "$SCRIPT_DIR/.." || { echo "fatal error" >&2; exit 1; }
 cargo update
-cargo test --features "bundled buildtime_bindgen"
+cargo test --features "extensions-full buildtime_bindgen"
 printf '    \e[35;1mFinished\e[0m bundled DUCKDB tests\n'
