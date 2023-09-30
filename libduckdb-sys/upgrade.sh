622e24e597f04bd196b646c23ec0fb75f3ffec78
wangfenjin <wangfenj@gmail.com>
2023-09-30 15:59:14 +0800

    ./upgrade.sh 0.8.1 0.9.0

--- libduckdb-sys/upgrade.sh
+++ libduckdb-sys/upgrade.sh
@@ -10,11 +10,11 @@ export DUCKDB_LIB_DIR="$SCRIPT_DIR/duckdb"
 export DU_INCLUDE_DIR="$DUCKDB_LIB_DIR"
 
 # Download and extract amalgamation
-DUCKDB_VERSION=v0.8.1
+DUCKDB_VERSION=v0.9.0
 git submodule update --init --checkout
 cd "$SCRIPT_DIR/duckdb-sources" || { echo "fatal error" >&2; exit 1; }
 git fetch
-git switch "$DUCKDB_VERSION"
+git checkout "$DUCKDB_VERSION"
 cd "$SCRIPT_DIR" || { echo "fatal error" >&2; exit 1; }
 python3 "$SCRIPT_DIR/update_sources.py"
 
