30ff0057af574ca18c51178afedc4b3b6f524b19
martin <martincharles07@gmail.com>
2024-08-16 15:23:25 -0500

    add todo

--- crates/libduckdb-sys/upgrade.sh
+++ crates/libduckdb-sys/upgrade.sh
@@ -10,6 +10,7 @@ mkdir -p "$SCRIPT_DIR/../../target" "$SCRIPT_DIR/duckdb"
 export DUCKDB_LIB_DIR="$SCRIPT_DIR/duckdb"
 
 # Download and extract amalgamation
+# todo: fix this version
 DUCKDB_VERSION=77a322a4cc42416b7933dc49affe0024d34eebc0
 git submodule update --init --checkout
 cd "$SCRIPT_DIR/duckdb-sources"
