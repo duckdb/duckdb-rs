1a895db8ead92d6460ccf3b9bf829e1b1b55d558
peasee <98815791+peasee@users.noreply.github.com>
2024-11-13 13:27:14 +1000

    fix: Support boolean lists

--- crates/duckdb/src/vtab/arrow.rs
+++ crates/duckdb/src/vtab/arrow.rs
@@ -525,7 +525,7 @@ fn list_array_to_vector<O: OffsetSizeTrait + AsPrimitive<usize>>(
     let value_array = array.values();
     let mut child = out.child(value_array.len());
     match value_array.data_type() {
-        dt if dt.is_primitive() => {
+        dt if dt.is_primitive() || matches!(dt, DataType::Boolean) => {
             primitive_array_to_vector(value_array.as_ref(), &mut child)?;
         }
         DataType::Utf8 => {
@@ -556,7 +556,7 @@ fn fixed_size_list_array_to_vector(
     let value_array = array.values();
     let mut child = out.child(value_array.len());
     match value_array.data_type() {
-        dt if dt.is_primitive() => {
+        dt if dt.is_primitive() || matches!(dt, DataType::Boolean) => {
             primitive_array_to_vector(value_array.as_ref(), &mut child)?;
         }
         DataType::Utf8 => {
