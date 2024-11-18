7ba911bec65a07b9b186624b1558903693eb9c9a
Nolan Darilek <nolan@thewordnerd.info>
2024-11-18 14:21:35 -0600

    Support building on Android
    * Remove `-stdlib=libstdc++` flag on Android since it breaks the build
    * Add `bundled-android-static-libstdcpp` feature for selecting the static C++ runtime (I.e. https://developer.android.com/ndk/guides/cpp-support#use_clang_directly)

--- crates/libduckdb-sys/build.rs
+++ crates/libduckdb-sys/build.rs
@@ -37,6 +37,7 @@ fn main() {
 mod build_bundled {
     use std::{
         collections::{HashMap, HashSet},
+        env,
         path::Path,
     };
 
@@ -152,6 +153,7 @@ mod build_bundled {
 
         cfg.cpp(true)
             .flag_if_supported("-std=c++11")
+            .flag_if_supported("-stdlib=libc++")
             .flag_if_supported("/bigobj")
             .warnings(false)
             .flag_if_supported("-w");
@@ -161,8 +163,19 @@ mod build_bundled {
             Ok(v) => v != "false" && v != "0",
             Err(_) => false,
         };
+
         if !is_debug {
             cfg.define("NDEBUG", None);
+        }
+
+        // The Android NDK doesn't build with this flag set.
+        if env::var("CARGO_CFG_TARGET_OS").unwrap() != "android" {
+            cfg.flag_if_supported("-stdlib=libstdc++");
+        }
+
+        #[cfg(feature = "bundled-android-static-libstdcpp")]
+        if env::var("CARGO_CFG_TARGET_OS").unwrap() == "android" {
+            cfg.flag_if_supported("-static-libstdc++");
         }
 
         if win_target() {
