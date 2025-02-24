5eeb1f01c278790ce1e2d24045f0096e9e2528e4
tk04 <turkisaloufi@gmail.com>
2025-02-24 11:17:58 -0500

    bundle core_functions extension (#437)

--- crates/libduckdb-sys/build.rs
+++ crates/libduckdb-sys/build.rs
@@ -125,6 +125,8 @@ mod build_bundled {
 
         let mut cfg = cc::Build::new();
 
+        add_extension(&mut cfg, &manifest, "core_functions", &mut cpp_files, &mut include_dirs);
+
         #[cfg(feature = "parquet")]
         add_extension(&mut cfg, &manifest, "parquet", &mut cpp_files, &mut include_dirs);
 
