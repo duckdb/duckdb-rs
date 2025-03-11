3d018388fc970beb810dacd845bf8b6228cded16
Max Gabrielsson <max@gabrielsson.com>
2025-03-11 11:57:12 +0100

    bundle ICU as part of extensions-full

--- crates/libduckdb-sys/build.rs
+++ crates/libduckdb-sys/build.rs
@@ -133,6 +133,9 @@ mod build_bundled {
         #[cfg(feature = "json")]
         add_extension(&mut cfg, &manifest, "json", &mut cpp_files, &mut include_dirs);
 
+        #[cfg(feature = "icu")]
+        add_extension(&mut cfg, &manifest, "icu", &mut cpp_files, &mut include_dirs);
+
         // duckdb/tools/pythonpkg/setup.py
         cfg.define("DUCKDB_EXTENSION_AUTOINSTALL_DEFAULT", "1");
         cfg.define("DUCKDB_EXTENSION_AUTOLOAD_DEFAULT", "1");
