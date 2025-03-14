a87ed8274d1e8c105b5b58a844b2f1ecee439144
Joe Isaacs <joe.isaacs@live.co.uk>
2025-03-14 17:10:51 +0000

    impl with macro

--- crates/duckdb/src/core/value.rs
+++ crates/duckdb/src/core/value.rs
@@ -1,6 +1,8 @@
 use crate::ffi::{duckdb_destroy_value, duckdb_get_int64, duckdb_get_varchar, duckdb_value};
 use libduckdb_sys::{
-    duckdb_create_bool, duckdb_create_int32, duckdb_create_int64, duckdb_create_null_value, duckdb_create_uint64,
+    duckdb_create_bool, duckdb_create_double, duckdb_create_float, duckdb_create_int16, duckdb_create_int32,
+    duckdb_create_int64, duckdb_create_int8, duckdb_create_null_value, duckdb_create_uint16, duckdb_create_uint32,
+    duckdb_create_uint64, duckdb_create_uint8,
 };
 use std::{ffi::CString, fmt};
 
@@ -23,14 +25,6 @@ where
     }
 }
 
-impl From<bool> for Value {
-    fn from(value: bool) -> Self {
-        Value {
-            ptr: unsafe { duckdb_create_bool(value) },
-        }
-    }
-}
-
 impl Value {
     pub fn null() -> Value {
         Value {
@@ -42,30 +36,6 @@ impl Value {
 impl From<duckdb_value> for Value {
     fn from(ptr: duckdb_value) -> Self {
         Self { ptr }
-    }
-}
-
-impl From<i32> for Value {
-    fn from(t: i32) -> Self {
-        Self {
-            ptr: unsafe { duckdb_create_int32(t) },
-        }
-    }
-}
-
-impl From<i64> for Value {
-    fn from(t: i64) -> Self {
-        Self {
-            ptr: unsafe { duckdb_create_int64(t) },
-        }
-    }
-}
-
-impl From<u64> for Value {
-    fn from(t: u64) -> Self {
-        Self {
-            ptr: unsafe { duckdb_create_uint64(t) },
-        }
     }
 }
 
@@ -93,3 +63,27 @@ impl fmt::Display for Value {
         write!(f, "{}", c_string.to_str().unwrap())
     }
 }
+
+macro_rules! impl_duckdb_create_value {
+    ($ty:ty, $ddb_fn:ident) => {
+        impl From<$ty> for Value {
+            fn from(value: $ty) -> Self {
+                Value {
+                    ptr: unsafe { $ddb_fn(value) },
+                }
+            }
+        }
+    };
+}
+
+impl_duckdb_create_value!(bool, duckdb_create_bool);
+impl_duckdb_create_value!(i8, duckdb_create_int8);
+impl_duckdb_create_value!(i16, duckdb_create_int16);
+impl_duckdb_create_value!(i32, duckdb_create_int32);
+impl_duckdb_create_value!(i64, duckdb_create_int64);
+impl_duckdb_create_value!(u8, duckdb_create_uint8);
+impl_duckdb_create_value!(u16, duckdb_create_uint16);
+impl_duckdb_create_value!(u32, duckdb_create_uint32);
+impl_duckdb_create_value!(u64, duckdb_create_uint64);
+impl_duckdb_create_value!(f32, duckdb_create_float);
+impl_duckdb_create_value!(f64, duckdb_create_double);
