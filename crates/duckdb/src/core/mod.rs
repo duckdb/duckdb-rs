8d142ba93fa98dd26f743cb9c12cfe647e2dca7d
Omer Ozarslan <omerfaruko@gmail.com>
2025-12-04 18:41:57 -0800

    Add ValueHandle, a wrapper around duckdb_value
    This will be useful later when calling FFI, in particular, for
    duckdb_create_list_value and duckdb_bind_value.

--- crates/duckdb/src/core/mod.rs
+++ crates/duckdb/src/core/mod.rs
@@ -1,7 +1,9 @@
 mod data_chunk;
 mod logical_type;
+mod value;
 mod vector;
 
 pub use data_chunk::DataChunkHandle;
 pub use logical_type::{LogicalTypeHandle, LogicalTypeId};
+pub use value::ValueHandle;
 pub use vector::*;
